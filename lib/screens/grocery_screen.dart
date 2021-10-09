3d2ee0ea16c6237212c62725035afdf1b9d6adfd
ivan-boginski <aisivan36@gmail.com>
2021-10-10 01:01:04 +0700

    some changes

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:fooderlich/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
+import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO add a scafold widget
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      /// floatingActionButton
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          /// TODO Present GroceryItemScreen
+        },
+      ),
+      body: buildGroceryScreen(), // Show the buildGroceryScreen
+    );
   }
-  // TODO add buildGroceryScreen
+
+  /// [buildGroceryScreen()] is a helper function that decides which widget tree
+  /// to construct. Here’s how it works:
+  /// 1 We wrap our widgets inside a Consumer,
+  ///  which listens for [GroceryManager] state changes.
+  /// 2 Consumer rebuilds the widgets below itself when the grocery -
+  /// items changes.
+  /// 3 If there are grocery items in the list, show the [GroceryListScreen].
+  /// 4 If there are no grocery items, show the [EmptyGroceryScreen].
+  ///  ====================== [Note that] ======================================
+  /// We should only wrap a Consumer around widgets that need it.
+  /// For example, wrapping a consumer widgets at the top level would force it -
+  /// to rebuild the entire tree!.
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(builder: (context, manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        //TODO Add groceryListScreen
+        return Container();
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    });
+  }
 }
